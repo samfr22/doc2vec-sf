ce70bdd688ad44caa6bca005ba3a01e534f57b58
Samuel Fritz <sfritz96@gwu.edu>
2022-11-21 22:34:55 +0100

    Additional code outline

--- doc2vec.py
+++ doc2vec.py
@@ -1,7 +1,26 @@
+import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import Dense
+
 class Doc2Vec():
     """
-    
+    3 layers NN
+        - Input layer with n nodes - n == number of documents, made with one-hot
+          encoding for the document number
+        - Hidden layer with size p - p == size of paragraph vectors outputted
+        - Activation function is weighted sum of input layer activations
+        - Output layer with M  - M == number of unique words in vocab
+
+    Run a softmax to get a classification 
+
+    Paragraph vector refers to the p x n matrix with weights
     """
 
     def __init__():
+        pass
+
+    def train(data):
+        pass
+
+    def predict():
         pass
---
+++ main.py
@@ -0,0 +1,9 @@
+import doc2vec
+
+def prepare_data():
+    """
+    Prepare the data for use in the model - download the csv and split into
+    text tokens
+    """
+    pass
+
